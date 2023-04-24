bca73cd663b06559849078c37ebab226e222e599
Alperen Kılıç <alp.kilic.97@gmail.com>
2023-04-25 00:12:12 +0300

    favvvv

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,10 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "ChatGPT Günlük Hava Durumu",
   description: "ChatGPT Günlük Hava Durumu",
+  icons: {
+    icon: { url: "/favicon.svg", type: "image/svg" },
+    shortcut: { url: "/favicon.svg", type: "image/svg" },
+  },
 };
 
 export default function RootLayout({
